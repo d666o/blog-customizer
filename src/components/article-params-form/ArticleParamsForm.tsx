7a6985e8f7d57f82c55f4a654403a642c0deb69e
Maksim Kharitonov <d.666o@yandex.ru>
2026-06-24 15:57:58 +0300

    сдача проекта

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,15 +2,116 @@ import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { useState } from 'react';
+import clsx from 'clsx';
+import { Select } from 'src/ui/select';
+import {
+	ArticleStateType,
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+	OptionType,
+} from 'src/constants/articleProps';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	style: ArticleStateType;
+	styleHandler: (selected: ArticleStateType) => void;
+};
+
+export const ArticleParamsForm = (props: ArticleParamsFormProps) => {
+	const { style, styleHandler } = props;
+	const [isOpen, setIsOpen] = useState(false);
+	const [selectedStyles, setSelectedStyles] = useState({
+		fontFamilyOption: style.fontFamilyOption,
+		fontColor: style.fontColor,
+		backgroundColor: style.backgroundColor,
+		contentWidth: style.contentWidth,
+		fontSizeOption: style.fontSizeOption,
+	});
+	const setSelectedStylesHandler = (selected: OptionType, type: string) => {
+		setSelectedStyles({ ...selectedStyles, [type]: selected });
+	};
+	const openHandler = () => {
+		isOpen && setIsOpen(false);
+		!isOpen && setIsOpen(true);
+	};
+	const resetHandler = () => {
+		styleHandler(defaultArticleState);
+		setSelectedStyles(defaultArticleState);
+	};
+	const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		styleHandler(selectedStyles);
+	};
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton
+				isOpen={isOpen}
+				onClick={() => {
+					openHandler();
+				}}
+			/>
+			<aside
+				className={clsx(styles.container, { [styles.container_open]: isOpen })}>
+				<form className={styles.form} onSubmit={submitHandler}>
+					<Select
+						selected={selectedStyles.fontFamilyOption}
+						options={fontFamilyOptions}
+						placeholder='Выберите шрифт'
+						onChange={(selected) =>
+							setSelectedStylesHandler(selected, 'fontFamilyOption')
+						}
+						title='шрифт'
+					/>
+					<RadioGroup
+						name='radio'
+						options={fontSizeOptions}
+						selected={selectedStyles.fontSizeOption}
+						onChange={(selected) =>
+							setSelectedStylesHandler(selected, 'fontSizeOption')
+						}
+						title='размер шрифта'
+					/>
+					<Select
+						selected={selectedStyles.fontColor}
+						options={fontColors}
+						placeholder='Выберите цвет шрифта'
+						onChange={(selected) =>
+							setSelectedStylesHandler(selected, 'fontColor')
+						}
+						title='цвет шрифта'
+					/>
+					<Separator />
+					<Select
+						selected={selectedStyles.backgroundColor}
+						options={backgroundColors}
+						placeholder='Выберите цвет фона'
+						onChange={(selected) =>
+							setSelectedStylesHandler(selected, 'backgroundColor')
+						}
+						title='цвет фона'
+					/>
+					<Select
+						selected={selectedStyles.contentWidth}
+						options={contentWidthArr}
+						placeholder='Выберите ширину контента'
+						onChange={(selected) =>
+							setSelectedStylesHandler(selected, 'contentWidth')
+						}
+						title='ширина контента'
+					/>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
+						<Button
+							title='Сбросить'
+							onClick={resetHandler}
+							htmlType='reset'
+							type='clear'
+						/>
 						<Button title='Применить' htmlType='submit' type='apply' />
 					</div>
 				</form>
